7ef51d07ab584f7fb6359b8b6f4ffd1065993fd2
zjupure <zjupure@gmail.com>
2015-07-03 10:36:28 +0800

    add WallPaperCrawler

---
+++ src/example/demo/WallPaperCrawler.java
@@ -0,0 +1,27 @@
+package example.demo;
+
+import example.http.Crawler;
+
+public class WallPaperCrawler extends Crawler {
+	private static String base_url = "http://wordsmotivate.me/";
+	private static String base_domain = "wordsmotivate";
+	private static String site_tag = "WallPaper";
+	
+	private static String img_lable = "div.post_img img";
+	
+	
+	public WallPaperCrawler() {
+		// TODO Auto-generated constructor stub
+		super(base_url, base_domain, site_tag);
+		
+		setImageLable(img_lable);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WallPaperCrawler mCrawler = new WallPaperCrawler();
+		
+		mCrawler.execute();
+	}
+
+}
